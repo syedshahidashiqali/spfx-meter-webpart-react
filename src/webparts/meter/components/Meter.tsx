95b06fcf163f0d49df666fa06a1fc6acad236174
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-05-16 14:03:14 +0500

    complete donut component

--- src/webparts/meter/components/Meter.tsx
+++ src/webparts/meter/components/Meter.tsx
@@ -1,22 +1,15 @@
 import * as React from 'react';
 import styles from './Meter.module.scss';
 import { IMeterProps } from './IMeterProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import Donut from './Donut';
 
 export default class Meter extends React.Component<IMeterProps, {}> {
   public render(): React.ReactElement<IMeterProps> {
-    const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName
-    } = this.props;
-
     return (
-      <section className={`${styles.meter} ${hasTeamsContext ? styles.teams : ''}`}>
+      <section className={styles.meter}>
         <h1>{ this.props.title }</h1>
-        <p>{ this.props.description }</p>
+        <p>{this.props.description}</p>
+        <Donut percentage={this.props.percentage}/>
       </section>
     );
   }

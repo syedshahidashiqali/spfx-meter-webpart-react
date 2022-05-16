aab7315686e2427026a8b44bd0d368d0325797f6
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-05-16 13:02:22 +0500

    initialize project

---
+++ src/webparts/meter/components/IMeterProps.ts
@@ -0,0 +1,7 @@
+export interface IMeterProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/meter/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IMeterWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'MeterWebPartStrings' {
+  const strings: IMeterWebPartStrings;
+  export = strings;
+}
